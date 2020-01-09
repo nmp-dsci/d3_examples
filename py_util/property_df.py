f05923528ff8550cf028b5880037f82d5010c2f5
Nathan Phillips <Nathan.Phillips@mynrma.com.au>
2020-01-09 16:54:15 +1100

    profile tool

--- py_util/property_df.py
+++ py_util/property_df.py
@@ -6,7 +6,7 @@ import numpy as np
 
 pd.options.display.max_columns = 100
 
-df = pd.read_csv('20180304_final_df.csv')
+df = pd.read_csv('20180304_final_df.csv/20180304_final_df.csv')
 
 ## time cutoff
 df = df.query("YYYYQ >=  20131 and YYYYQ <= 20174")
@@ -20,20 +20,26 @@ columns = ["source","state",'postcode',"suburb","dateID",
 
 df = df[columns]
 
-df.to_csv("20180304_property.csv")
-
-### 
+###  Prepare cuts of the data
 df.baths = df.baths.apply(lambda x: 5 if x > 5 else 0 if x < 0 else x  )
 df.beds = df.beds.apply(lambda x: 4 if x > 4 else  0 if x < 0 else x  )
 df.parking = df.parking.apply(lambda x: 3 if x > 3 else  0 if x < 0 else x  )
 
+###
+df.sale_price = df.sale_price.apply(lambda x: np.NaN if x < 300000 or x > 10000000 else x )
 
 
 ###  postcode summ 
-baseID = ['postcode']
+df['year'] = df.dateID.str.slice(0,4)
+
+baseID = ['postcode', 'year']
 aggID = ['baths','beds','parking','propertyType']
-poa_summ = df.query('sale_price==sale_price'
-    ).groupby(baseID+aggID
+
+
+df = df.query('sale_price==sale_price')
+
+
+poa_summ = df.groupby(baseID+aggID
     ).agg({'sale_price':[np.size,np.sum]})
 poa_summ.columns = ['volume','sales']
 poa_summ = poa_summ.reset_index()
@@ -44,12 +50,6 @@ poa_summ.beds = poa_summ.beds.astype(int)
 poa_summ.parking = poa_summ.parking.astype(int) 
 poa_summ.volume = poa_summ.volume.astype(int) 
 
-## put in overall
-overall = poa_summ.groupby(aggID)[['volume','sales']].sum().reset_index()
-overall['postcode'] = 9999
-
-poa_summ = pd.concat([poa_summ,overall],axis=0)
-
 ## left join back to postcode
 poa_master = pd.read_csv("mesh2poa.csv")
 poa_master = poa_master.query('POA_CODE_2016 >=2000 and POA_CODE_2016 <= 2999')
@@ -58,8 +58,6 @@ poa_master = poa_master.groupby('POA_CODE_2016').AREA_ALBERS_SQKM.sum().reset_in
 poa_master = poa_master.rename(columns={'POA_CODE_2016':'postcode',"AREA_ALBERS_SQKM":"sqkm"})
 
 poa_summ = poa_master.merge(poa_summ, on='postcode',how='left')
-
-poa_summ.query('sales != sales')
 
 poa_summ.to_csv("property_poa.csv")
 
